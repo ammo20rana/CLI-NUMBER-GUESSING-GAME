8322985fe3a769ff4d3ff97246596b4d0e6b3109
ammo20rana <167557167+ammo20rana@users.noreply.github.com>
2024-04-24 19:58:31 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+import inquirer from "inquirer";
+// 1)computer will generate a random number
+// 2)user input for guessing number
+// 3)compare user input with computer generated number and show result
+const RandomNumber = 13;
+const answers = await inquirer.prompt([
+    {
+        name: "userguessednumber",
+        type: "number",
+        message: "please guess a number between 1-10:",
+    }
+]);
+if (answers.userguessednumber === RandomNumber) {
+    console.log("congtratulation! you guessed right number.");
+}
+else {
+    console.log("you guessed wrong number.");
+}
